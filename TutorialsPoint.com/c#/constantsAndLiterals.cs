633d95dfd492105fe73694901d55818f8be7f74a
Unknown <nathanstasin@gmail.com>
2017-06-04 21:43:47 +0200

    sunday

--- TutorialsPoint.com/c#/constantsAndLiterals.cs
+++ TutorialsPoint.com/c#/constantsAndLiterals.cs
@@ -1,10 +1,11 @@
 /*Write a program which asks the user to enter a radius. The program then calculates the area of the circle and prints it out.
+Make sure that the area which is printed out only has two decimal places.
 
 Sample Output:
 Enter Radius
 13
 
-Radius: 13, Area: 530.92871
+Radius: 13, Area: 530.93
 */
 
 using System;
@@ -27,7 +28,7 @@ namespace DeclaringConstants
             //OR
             double areaCircle = pi*Math.Pow(r,2);//Math.Pow(base, exponent)
             
-            Console.WriteLine("Radius: {0}, Area: {1}", r, areaCircle);//Here the first parameter is a literal which has N substitution positions. 
+            Console.WriteLine("Radius: {0}, Area: {1:0.00}", r, areaCircle);//Here the first parameter is a literal which has N substitution positions. 
             //The next N parameters are inserted into those positions.
         }
     }
