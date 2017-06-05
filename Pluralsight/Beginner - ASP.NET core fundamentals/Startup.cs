5313176afff23bf4bad3401f38b4257025908bb4
Unknown <nathanstasin@gmail.com>
2017-06-05 11:51:25 +0200

    Up to minute 4:47 on Section 2, Adding a Configuration Source

--- Pluralsight/Beginner - ASP.NET core fundamentals/Startup.cs
+++ Pluralsight/Beginner - ASP.NET core fundamentals/Startup.cs
@@ -7,11 +7,21 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;//namespace for ConfigurationBuilder
+//also need to add the Microsoft.Extensions.Configuration.FileExtensions dependency
 
 namespace OdeToFood
 {
     public class Startup
     {
+
+        public Startup(){//constructor. Exucutes a bit of code before ConfigureServices and Configure execute
+            //Need to tell asp.net about your appsettings.json file
+            var builder = new ConfigurationBuilder ()//creating an instance of a class called ConfigurationBuilder
+            //It is in a namespace which is not being used by default, so need to add an appropriate using statement.
+                .SetBasePath()//Tell it where the base path is. But you don't want to hardCode anything
+
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)//setup Inversion of Control and Dependency injection.
