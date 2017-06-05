5a8dd7db7c477ff853606dc4810961c917488a96
Unknown <nathanstasin@gmail.com>
2017-06-05 15:08:29 +0200

    Start of section 3, Startup and middleware

--- Pluralsight/Beginner - ASP.NET core fundamentals/Greeter.cs
+++ Pluralsight/Beginner - ASP.NET core fundamentals/Greeter.cs
@@ -1,26 +1,32 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace OdeToFood
 {
-    //the interface describes the capabilities of every Greeter.
+    public interface IGreeter //the interface describes the capabilities of every Greeter.
     //An interface is defined as a syntactical contract that all the classes inheriting the interface should follow. 
     //The interface defines the 'what' part of the syntactical contract and the deriving classes define the 'how' part of the syntactical contract.
     //Interfaces contain only the declaration of the members. It is the responsibility of the deriving class to define the members. 
     //It often helps in providing a standard structure that the deriving classes would follow.
     //Interface statements are public by default.
     //We are using the concept of an interface so that the implementation is not tied to a particular implementation of a Greeter. So, interface doesn't care if info comes from a JSON file or an XML web service
-    public interface IGreeter 
     {
         string GetGreeting();//Every Greeter has the ability to get a greeting.
     }
     public class Greeter : IGreeter
     {
+        private string _greeting;
+
+        public Greeter(IConfiguration configuration)
+        {
+            _greeting = configuration["Greeting"];
+        }
         public string GetGreeting()
         {
-            return "Hello from Greeter!";
+            return _greeting;
         }
     }
 }
--- Pluralsight/Beginner - ASP.NET core fundamentals/Startup.cs
+++ Pluralsight/Beginner - ASP.NET core fundamentals/Startup.cs
@@ -38,10 +38,17 @@ namespace OdeToFood
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)//setup Inversion of Control and Dependency injection.
         {
+            //Need to create a service which implements IGreeter registered in the Services collection.
+            services.AddSingleton(Configuration);//ASP.NET knows the type of Configuration and so, any object which needs IConfiguration, it will use the object on this line.
+            services.AddSingleton<IGreeter, Greeter>();//This allows the implentation of the interface for the Greeter method. This is saying that whenever you see something which needs the IGreeter parameter, instantiate the Greeter class and pass in the IGreeter
+            //Once again, this allows you to use different types of IGreeter. One could connect to a database or a json or whatever. Only this line knows what this particular IGreeter is connecting to.
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, 
+            IHostingEnvironment env, 
+            ILoggerFactory loggerFactory, 
+            IGreeter greeter)//method now takes an Igreeter called greeter
         {
             loggerFactory.AddConsole();
 
@@ -54,7 +61,11 @@ namespace OdeToFood
             {
                 //instead of the hardcoded string below:
                 //await context.Response.WriteAsync("Hello World!!!!!!!!!!! Hello");
-                var message = Configuration["Greeting"];//Going into the Configuration property (builder.Build()) and then find the message based on the index in the appsettings.json file 
+                //doesnt want the code on the inside of the Configuration method to access the configuration source directly.
+                //Instead, you want the method to go through some application service. So, an object will retrieve the information. Need to add a class called Greeter.cs
+
+                //Now, instead of going to configuration source, will now get the greeting from this greeter. That message will then be written into every http request
+                var message =greeter.GetGreeting();//Going into the Configuration property (builder.Build()) and then find the message based on the index in the appsettings.json file 
                 //So, will look for value associated with key of "Greeting"
                 await context.Response.WriteAsync(message);
             });
