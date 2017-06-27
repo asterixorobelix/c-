f747c754fa15c2576ee20f0fb29b2b4eda18a376
Unknown <nathanstasin@gmail.com>
2017-06-27 14:06:40 +0200

    Modified page83 to take user input

--- Rob Miles Book/Page83.cs
+++ Rob Miles Book/Page83.cs
@@ -2,12 +2,10 @@
 Page 83 of Rob Miles c# book.
 Code Sample 23
 
-Create a simple BankProgram. The BankProgram contains various accounts, which contain the following information for each customer:
+Create a simple BankProgram, using a struct. The BankProgram contains various accounts, which contain the following information for each customer:
 customer name - string
 customer address - string
-account number - integer value
 account balance - integer value
-overdraft limit - integer value
 Bank Accounts can also be "Frozen", "New", "Active", "Closed" and "Under Audit".
 
 Create an Account called RobsAccount, set the account state to Active and give it a balance of a million Rand.
@@ -18,34 +16,31 @@ Robs balance is: 1000000 Rand
 
 using System;
 
-enum AccountState
-{
-    New,
-    Active,
-    UnderAudit,
-    Frozen,
-    Closed
-} ;
+enum State{
+	Frozen,New,Active,Closed,UnderAudit
+};
 
-struct Account
-{
-    public AccountState State;
-    public string Name;
-    public string Address;
-    public int AccountNumber;
-    public int Balance;
-    public int Overdraft;
-} ;
+struct Account{
+	public string Name;
+	public string Address;
+	public int Balance;
+	public State AccountState;
+};
 
-class BankProgram
-{
-
-    public static void Main()
-    {
-        Account RobsAccount;
-        RobsAccount.State = AccountState.Active;
-        RobsAccount.Balance = 1000000;
-
-        Console.WriteLine("Robs balance is: {0} Rand",RobsAccount.Balance);
-    }
+class BankProgram{
+	static void Main(){
+		Console.WriteLine("Enter your details in order to create an account");
+		Account Account1;
+		Account1.AccountState=State.New;
+		Console.WriteLine("Enter your name:");
+		Account1.Name=Console.ReadLine();
+		Console.WriteLine("Enter your address: ");
+		Account1.Address=Console.ReadLine();
+		Console.WriteLine("What is your opening balance?");
+		Account1.Balance=int.Parse(Console.ReadLine());
+		
+		Console.WriteLine("\nYour account details");
+		Console.WriteLine("Name: "+Account1.Name);
+		Console.WriteLine("Balance: "+Account1.Balance);		
+	}
 }
