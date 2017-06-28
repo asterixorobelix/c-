87f70b435a4383f23188b41ba10afad96a8f3f5f
Unknown <nathanstasin@gmail.com>
2017-06-28 17:01:51 +0200

    adf

--- Online C# lessons/BlackWasp2.cs
+++ Online C# lessons/BlackWasp2.cs
@@ -1,25 +1,18 @@
 /*
 http://www.blackwasp.co.uk/CSharpSimpleClass_2.aspx
-Create a class called Vehicle.
-Create a new vehicle object and run its ToString method to see the results. As we have not yet defined how ToString should work, this will simply show the fully qualified name.
-Add a new methodto the vehicle class for pressing a vehicle's horn
+Create a class called Vehicle. Add new method called PressHorn to the vehicle class for pressing a vehicle's horn.
+Create a new vehicle object and run its PressHorn method to see the results. 
 */
 using System;
 
-namespace ClassTest
-{
-    class Vehicle
-    {
-		public void PressHorn()
-		{
-			Console.WriteLine("Toot toot!");
-		}
-		
-		static void Main()
-		{
-			Vehicle car = new Vehicle();
-			Console.WriteLine(car.ToString()); // Outputs "ClassTest.Vehicle"
-			car.PressHorn(); // Outputs "Toot toot!"
-		}
-    }
+class Vehicle{
+	
+	void PressHorn(){
+		Console.WriteLine("Beep");
+	}
+	
+	static void Main (){
+		Vehicle Beetle = new Vehicle();
+		Beetle.PressHorn();
+	}
 }
