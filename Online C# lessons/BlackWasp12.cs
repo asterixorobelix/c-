8359efc35570d96ac9f611e6e286fc9b2da2b77e
Unknown <nathanstasin@gmail.com>
2017-06-29 14:00:45 +0200

    adfs

---
+++ Online C# lessons/BlackWasp12.cs
@@ -0,0 +1,136 @@
+using System;
+
+namespace InheritanceDemo
+{
+	public class Vehicle
+	{
+		protected int _speed;     // Miles per hour. A protected member is accessible within its class and by derived class instances.
+
+		public int Speed
+		{
+			get
+			{
+				return _speed;
+			}
+		}
+
+		public void Accelerate(int mph)
+		{
+			_speed += mph;
+		}
+
+		public void Decelerate(int mph)
+		{
+			_speed -= mph;
+		}
+
+		public virtual void Indicate(bool turningLeft)
+		{
+			if (turningLeft)
+				Console.WriteLine("Turning left");
+			else
+				Console.WriteLine("Turning right");
+		}
+	}
+	
+	class MotorVehicle : Vehicle
+    {
+        private float _fuelRemaining;
+        private float _tankSize;
+
+
+        public float FuelRemaining
+        {
+            get
+            {
+                return _fuelRemaining;
+            }
+        }
+
+        public int Mpg
+        {
+            get
+            {
+                if (_speed == 0)
+                    return 0;
+                else if (_speed < 20)
+                    return 50;
+                else if (_speed < 50)
+                    return 40;
+                else
+                    return 35;
+            }
+        }
+
+        public float TankSize
+        {
+            get
+            {
+                return _tankSize;
+            }
+            set
+            {
+                _tankSize = value;
+            }
+        }
+
+        public float Refuel()
+        {
+            float fuelRequired = _tankSize - _fuelRemaining;
+            _fuelRemaining = _tankSize;
+            return fuelRequired;
+        }
+
+        public override void Indicate(bool turningLeft)
+        {
+            if (turningLeft)
+                Console.WriteLine("Flashing left indicator");
+            else
+                Console.WriteLine("Flashing right indicator");
+        }
+    }
+	
+	class Bicycle : Vehicle
+    {
+        public void RingBell()
+        {
+            Console.WriteLine("Ring!");
+        }
+
+
+        public override void Indicate(bool turningLeft)
+        {
+            base.Indicate(turningLeft);
+
+            if (turningLeft)
+                Console.WriteLine("Raising left arm");
+            else
+                Console.WriteLine("Raising right arm");
+        }
+
+    }
+		
+	class Program
+		{
+        static void Main(string[] args)
+        {
+            MotorVehicle car = new MotorVehicle();
+            Console.WriteLine("Speed: {0}mph", car.Speed);    // Outputs "Speed 0mph"
+            car.Accelerate(25);
+            Console.WriteLine("Speed: {0}mph", car.Speed);    // Outputs "Speed 25mph"
+            car.Decelerate(15);
+            Console.WriteLine("Speed: {0}mph", car.Speed);    // Outputs "Speed 10mph"
+						
+			car.TankSize = 11;
+			Console.WriteLine("Fuel: {0}g", car.FuelRemaining); // Outputs "0g"
+			car.Refuel();
+			Console.WriteLine("Fuel: {0}g", car.FuelRemaining); // Outputs "11g"
+			 
+			Bicycle bike = new Bicycle();
+			bike.RingBell();  // Outputs "Ring!"
+			car.Indicate(true); // Outputs "Flashing left indicator"
+ 			bike.Indicate(true);  //Turning left Raising left arm
+
+        }
+    }
+}
--- Online C# lessons/InheritanceDemo/InheritanceDemo/InheritanceDemo/Program.cs
+++ Online C# lessons/InheritanceDemo/InheritanceDemo/InheritanceDemo/Program.cs
@@ -10,6 +10,12 @@ namespace InheritanceDemo
     {
         static void Main(string[] args)
         {
+            MotorVehicle v = new MotorVehicle();
+            Console.WriteLine("Speed: {0}mph", v.Speed);    // Outputs "Speed 0mph"
+            v.Accelerate(25);
+            Console.WriteLine("Speed: {0}mph", v.Speed);    // Outputs "Speed 25mph"
+            v.Decelerate(15);
+            Console.WriteLine("Speed: {0}mph", v.Speed);    // Outputs "Speed 10mph"
             Console.ReadLine();
             // Add sample Main method code here.
         }
