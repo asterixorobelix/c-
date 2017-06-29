d2d36bbd6952ddc1d53a101c3a735bf888fe966d
Unknown <nathanstasin@gmail.com>
2017-06-29 09:44:06 +0200

    n/

--- Online C# lessons/BlackWasp7.cs
+++ Online C# lessons/BlackWasp7.cs
@@ -5,43 +5,28 @@ Add a method called Square, which calculates the square of an integer value.
 In order to support the double data type we can add a second variation of the method to the Calculate class. This overloaded method will accept and return doubles rather than integers. 
 
 Sample Output:
-Double Square calculated
-25
-Integer Square calculated
-25
-Double Square calculated
-25
+Calculating square of an integer: 16
+Calculating square of a double: 6,25
+Calculating square of a double: 25
  */
+
 using System;
 
-namespace CSharpMethodOverloading
-{
-	class Program
-    {
-		class Calculate
-		{
-			public static int Square(int number)
-			{
-				Console.WriteLine("Integer Square calculated");
-				return number * number;
-			}
-			
-			public static double Square(double number)
-			{
-				Console.WriteLine("Double Square calculated");
-				return number * number;
-			}
-		}
-		
-        static void Main(string[] args)
-        {
-            double squareMe = 5;
-            int squareMeToo = 5;
-            Console.WriteLine(Calculate.Square(squareMe));
-            Console.WriteLine(Calculate.Square(squareMeToo));
-
-            float squareMeThree = 5;
-            Console.WriteLine(Calculate.Square(squareMeThree)); //where an implicit cast is possible, the compiler will perform this conversion automatically. Although no overloaded method exists for floats the double variation is used instead.
-        }
-    }
+class Calculate{
+	
+	static int Square(int v){
+		Console.WriteLine("Calculating square of an integer: {0}", v*v);
+		return v*v;
+	}
+	
+	static double Square(double v){
+		Console.WriteLine("Calculating square of a double: {0}", v*v);
+		return v*v;
+	}
+	
+	static void Main(){
+		Square(4);
+		Square(2.5);
+		Square((float)5); //where an implicit cast is possible, the compiler will perform this conversion automatically. Although no overloaded method exists for floats the double variation is used instead.
+	}
 }
