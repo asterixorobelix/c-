43614acf3dc95581a9f0bea9760c1714847e7fea
nathan.stasin <nathanstasin@gmail.com>
2017-07-14 09:14:31 +0200

    Finished generic type questions

---
+++ Ebooks/Pat McGee/Chapter 10/10.2/Ex10point2/Ex10point2/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * pg 305, Example10.2- pat mcgee
+Create a generic QuizQuestion class which stores quiz questions and answers in varying data formats. 
+Create a QuizQuestion object which assigns an integer and a string to the generic type.
+
+Sample Output:
+Question How many shark species exist?
+A) 350
+B) None of the above
+*/
+namespace Ex10point2
+{
+    class Program
+    {
+        static void Main()
+        {
+            QuizQuestion<int, string> Q1 = new QuizQuestion<int,string>("How many shark species exist?", 350, "None of the above");
+            Q1.showQA();
+        }
+    }
+}
---
+++ Ebooks/Pat McGee/Chapter 10/10.2/Ex10point2/Ex10point2/QuizQuestion.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Ex10point2
+{
+    class QuizQuestion<T,U>
+    {
+        public T Response1 { get; set; }
+        public U Response2 { get; set; }
+        public string  Question { get; set; }
+
+        internal QuizQuestion(string Q,T a1, U a2)
+        {
+            Response1 = a1;
+            Response2 = a2;
+            Question = Q;
+        }
+
+        public void showQA() {
+            Console.WriteLine("Question {0}", Question);
+            Console.WriteLine("A) " + Response1);
+            Console.WriteLine("B) " + Response2);
+        }
+    }
+}
