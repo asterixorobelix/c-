5d8e54ee6442cb0a5a312d1902b6fa69d3043c69
Unknown <nathanstasin@gmail.com>
2017-07-18 16:39:52 +0200

    Added some details to Application class

--- Serenity/PracticeHelp/PracticeHelp.Data/Application.cs
+++ Serenity/PracticeHelp/PracticeHelp.Data/Application.cs
@@ -1,6 +1,14 @@
-﻿namespace PracticeHelp.Data
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PracticeHelp.Data
 {
     public class Application
     {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid ApplicationId { get; set; }
+        [MaxLength(100)]
+        public string ApplicationName { get; set; }
     }
 }
