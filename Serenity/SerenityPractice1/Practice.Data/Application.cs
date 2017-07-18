b6f41b011c789f17a5a6b1726802dd0c5125c9f4
nathan.stasin <nathanstasin@gmail.com>
2017-07-18 17:26:45 +0200

    Added some details to the Application class

--- Serenity/SerenityPractice1/Practice.Data/Application.cs
+++ Serenity/SerenityPractice1/Practice.Data/Application.cs
@@ -2,5 +2,9 @@
 {
     public class Application
     {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid ApplicationId { get; set; }
+        [MaxLength(100)]
+        public string ApplicationName { get; set; }
     }
 }
