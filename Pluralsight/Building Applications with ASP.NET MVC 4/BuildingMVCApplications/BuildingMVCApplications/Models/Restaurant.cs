c3bd4e270701331293b757af66d204da5ad942aa
nathan.stasin <nathanstasin@gmail.com>
2017-07-19 17:52:00 +0200

    Created Restaurant model

---
+++ Pluralsight/Building Applications with ASP.NET MVC 4/BuildingMVCApplications/BuildingMVCApplications/Models/Restaurant.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BuildingMVCApplications.Models
+{
+    public class Restaurant
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+
+        public ICollection<Review> Reviews { get; set; }
+        //An ICollection is a generic type version of a list
+    }
+}
