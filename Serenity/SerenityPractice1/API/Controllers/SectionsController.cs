cbf1f309c9b25f6d9a505d942e77095cbda8d941
nathan.stasin <nathanstasin@gmail.com>
2017-07-19 10:46:45 +0200

    Added database context for the SectionsController and reference to Data project with a using statement

--- Serenity/SerenityPractice1/API/Controllers/SectionsController.cs
+++ Serenity/SerenityPractice1/API/Controllers/SectionsController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Practice.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -9,5 +10,6 @@ namespace API.Controllers
 {
     public class SectionsController : ApiController
     {
+        DataModel db = new DataModel();
     }
 }
