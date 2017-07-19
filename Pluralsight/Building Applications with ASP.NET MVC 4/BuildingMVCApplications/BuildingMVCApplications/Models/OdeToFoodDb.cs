1ff6a5ec74fa4444fd78d3fd8163226213e47994
nathan.stasin <nathanstasin@gmail.com>
2017-07-19 18:09:55 +0200

    Added Entity Framework properties to the DbContext file

--- Pluralsight/Building Applications with ASP.NET MVC 4/BuildingMVCApplications/BuildingMVCApplications/Models/OdeToFoodDb.cs
+++ Pluralsight/Building Applications with ASP.NET MVC 4/BuildingMVCApplications/BuildingMVCApplications/Models/OdeToFoodDb.cs
@@ -8,5 +8,10 @@ namespace BuildingMVCApplications.Models
 {
     public class OdeToFoodDb : DbContext
     {
+        //Can have properties of type DbSet, which represent the entities which you want to query and save in the database.
+        //Each DbSet essentially represents a table in the db
+        public DbSet<Restaurant> Restaurants { get; set; }
+        //DbSet is a generic type
+        public DbSet<Review> Reviews { get; set; }
     }
 }
