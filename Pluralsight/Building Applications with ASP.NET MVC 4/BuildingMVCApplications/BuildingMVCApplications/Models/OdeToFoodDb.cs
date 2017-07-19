cbc3bef74d2234edd2824c4778af23f7b5c1a490
nathan.stasin <nathanstasin@gmail.com>
2017-07-19 18:41:03 +0200

    Db Constructor with name of connection string

--- Pluralsight/Building Applications with ASP.NET MVC 4/BuildingMVCApplications/BuildingMVCApplications/Models/OdeToFoodDb.cs
+++ Pluralsight/Building Applications with ASP.NET MVC 4/BuildingMVCApplications/BuildingMVCApplications/Models/OdeToFoodDb.cs
@@ -8,6 +8,11 @@ namespace BuildingMVCApplications.Models
 {
     public class OdeToFoodDb : DbContext
     {
+        public OdeToFoodDb() : base("DefaultConnection")//Constructor for the OdeToFoodDb class calls the DbContext base constructor with the specified name of the connection string.
+        {
+
+        }
+
         //Can have properties of type DbSet, which represent the entities which you want to query and save in the database.
         //Each DbSet essentially represents a table in the db
         public DbSet<Restaurant> Restaurants { get; set; }
