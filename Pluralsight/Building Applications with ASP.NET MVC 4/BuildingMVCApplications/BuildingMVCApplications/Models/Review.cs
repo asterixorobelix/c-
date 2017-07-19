d29723901c899de7a5c38853f67ec27da7040642
nathan.stasin <nathanstasin@gmail.com>
2017-07-19 17:57:18 +0200

    Added properties to the Review model

--- Pluralsight/Building Applications with ASP.NET MVC 4/BuildingMVCApplications/BuildingMVCApplications/Models/Review.cs
+++ Pluralsight/Building Applications with ASP.NET MVC 4/BuildingMVCApplications/BuildingMVCApplications/Models/Review.cs
@@ -1,6 +1,12 @@
-﻿namespace BuildingMVCApplications.Models
+﻿using System;
+
+namespace BuildingMVCApplications.Models
 {
     public class Review
     {
+        public Guid Id { get; set; }
+        public int Rating { get; set; }
+        public string ReviewText { get; set; }
+        public Guid RestaurantId { get; set; }
     }
 }
