b0ef0e2033caa10d6211629bfd0312be957afeed
nathan.stasin <nathanstasin@gmail.com>
2017-07-19 11:10:34 +0200

    Made Common.Types.HelpText class public and added reference to it in SectionsController

--- Serenity/SerenityPractice1/API/Controllers/SectionsController.cs
+++ Serenity/SerenityPractice1/API/Controllers/SectionsController.cs
@@ -5,11 +5,18 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using Common.Types;
 
 namespace API.Controllers
 {
     public class SectionsController : ApiController
     {
         DataModel db = new DataModel();
+
+        public IEnumerable<HelpText> Get()
+        {
+            return null;
+        }
     }
+
 }
--- Serenity/SerenityPractice1/Common/Types/HelpText.cs
+++ Serenity/SerenityPractice1/Common/Types/HelpText.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Common.Types
 {
-    class HelpText
+    public class HelpText
     {
         public string Text { get; set; }
     }
