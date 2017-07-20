f5f1d3ba5cab867de9001f9c968c356ce36dbef4
nathan.stasin <nathanstasin@gmail.com>
2017-07-20 16:47:23 +0200

    Created Review class

---
+++ Pluralsight/Building Applications with ASP.NET MVC 4/BuildingMVC4Applications/BuildingMvc4Applications/Data/Models/Review.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Models
+{
+    [Table("Reviews")]
+    class Review
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid ReviewId { get; set; }
+
+        [Required]
+        public int Rating { get; set; }
+        public string ReviewText { get; set; }
+        public string ReviewerName { get; set; }
+    }
+}
