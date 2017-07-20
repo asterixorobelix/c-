4bd3e2ed56940b024841931e22340ecbfe04cfe6
nathan.stasin <nathanstasin@gmail.com>
2017-07-20 15:22:34 +0200

    Typed Enable-Migrations in console for Data project

---
+++ Pluralsight/Building Applications with ASP.NET MVC 4/BuildingMVC4Applications/BuildingMVC4Applications.Data/Migrations/Configuration.cs
@@ -0,0 +1,31 @@
+namespace BuildingMVC4Applications.Data.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<BuildingMVC4Applications.Data.BuildingMVC4ApplicationsDb>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(BuildingMVC4Applications.Data.BuildingMVC4ApplicationsDb context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
