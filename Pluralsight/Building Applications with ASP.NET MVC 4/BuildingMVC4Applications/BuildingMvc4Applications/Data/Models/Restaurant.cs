eae3187898ebd2d90734430a5771de15bfd8ad5a
nathan.stasin <nathanstasin@gmail.com>
2017-07-20 16:27:01 +0200

    Created Restaurant class

---
+++ Pluralsight/Building Applications with ASP.NET MVC 4/BuildingMVC4Applications/BuildingMvc4Applications/Data/Models/Restaurant.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Models
+{
+    [Table("Restaurants")]
+    class Restaurant
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid RestaurantId { get; set; }
+
+        [Required]
+        public string RestaurantName { get; set; }
+        public string City { get; set; }
+        public string  Country { get; set; }
+    }
+}
