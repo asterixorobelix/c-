6d97cc49107f358f415194566fab5118fc34ee13
nathan.stasin <nathanstasin@gmail.com>
2017-07-20 10:05:25 +0200

    Added RestaurantReview class

---
+++ Pluralsight/Building Applications with ASP.NET MVC 4/SerenityOdeToFood/SerenityOdeToFood.Data/RestaurantReview.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerenityOdeToFood.Data
+{
+    class RestaurantReview
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid ReviewId { get; set; }
+
+        public string ReviewText { get; set; }
+
+        public int Rating { get; set; }
+
+        [Required]
+        public Guid RestaurantId { get; set; }
+
+        [ForeignKey("RestaurantId")]
+        public virtual Restaurant Restaurant { get; set; }
+    }
+}
