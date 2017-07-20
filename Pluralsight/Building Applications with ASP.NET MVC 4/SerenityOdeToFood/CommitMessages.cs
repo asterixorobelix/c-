183458accc04b81f732be298c85c3a5cca93f79b
nathan.stasin <nathanstasin@gmail.com>
2017-07-20 10:45:56 +0200

    Added Models and Controllers folders to Serenity

---
+++ Pluralsight/Building Applications with ASP.NET MVC 4/SerenityOdeToFood/CommitMessages.cs
@@ -0,0 +1,21 @@
+Initial Commit and ran website for the first time
+Added data project to solution. Used Nuget to add Entity Framework to data project
+Added db context class
+Made Data project point at the SerenityDb
+Created initial blank migration and did an Update-Database
+Added RestaurantReview class
+Added attributes to RestaurantReview class
+Added Restaurant class
+Added virtual DbSets for the two classes (Restaurants and RestaurantReview)
+Added migration to create the tables for Reviews and Restaurants
+Ran sergen to create pages for Restaurants and Reviews
+
+
+
+
+
+
+
+
+
+
