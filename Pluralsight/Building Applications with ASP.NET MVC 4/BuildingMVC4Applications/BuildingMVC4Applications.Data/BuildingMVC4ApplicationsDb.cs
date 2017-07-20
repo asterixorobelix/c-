25889be969914dfe824724d2efee96c97c4009d3
nathan.stasin <nathanstasin@gmail.com>
2017-07-20 15:48:20 +0200

    Added db context class to data project

---
+++ Pluralsight/Building Applications with ASP.NET MVC 4/BuildingMVC4Applications/BuildingMVC4Applications.Data/BuildingMVC4ApplicationsDb.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildingMVC4Applications.Data
+{
+    class BuildingMVC4ApplicationsDb
+    {
+    }
+}
